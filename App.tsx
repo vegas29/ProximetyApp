42b2118a3f8627fa6f0bf26df49f4f7849bf76a3
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2022-06-01 23:12:48 -0500

    Creacion de la app

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hola Proximity</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
